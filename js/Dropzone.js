a2124e3a7461ccf2a21f93876aaa7de85e42c972
foyou <3509538277@qq.com>
2024-05-29 19:16:38 +0800

    help of

--- js/Dropzone.js
+++ js/Dropzone.js
@@ -1,18 +1,10 @@
-document.getElementById('uploadForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    var file = document.getElementById('fileToUpload').files[0];
-    var formData = new FormData();
-    formData.append('file', file);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/upload.php', true);
-    xhr.onload = function() {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            console.log('文件上传成功');
-        } else {
-            console.error('文件上传失败');
-        }
-    };
-    xhr.send(formData);
-});
+Dropzone.options.myDropzone = {
+    paramName: "file", // 上传文件的参数名
+    maxFilesize: 100, // 限制文件大小，单位为MB
+    acceptedFiles: ".jpg,.png,.gif", // 允许的文件类型
+    init: function() {
+        this.on("success", function(file, response) {
+            console.log(response); // 上传成功后的响应数据
+        });
+    }
+};
